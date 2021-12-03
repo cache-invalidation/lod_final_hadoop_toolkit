3dff533ec6997fe52802760ea5221a9c151c912f
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-12-03 22:01:33 +0300

    name decider

--- mapper.py
+++ mapper.py
@@ -3,6 +3,35 @@ import sys
 from bs4 import BeautifulSoup
 from glob import glob
 
+names_mask = [  {'name': 'Eвгени', 'surname': 'Медведев'},
+                {'name': 'Игор', 'surname': 'Акинфеев'},
+                {'name': 'Александр', 'surname': 'Овечкин'},
+                {'name': 'Олег', 'surname': 'Тиньков'},
+                {'name': 'Ольг', 'surname': 'Наумов'},
+                {'name': 'Андре', 'surname': 'Андреев'},
+                {'name': 'Алексе', 'surname': 'Картынник'},
+                {'name': 'Игор', 'surname': 'Бухман'},
+                {'name': 'Юри', 'surname': 'Дуд'},
+                {'name': 'Григори', 'surname': 'Перельман'}]
+
+names = [   {'name': 'Eвгения', 'surname': 'Медведева', 'patronymic':'Армановна'},
+            {'name': 'Игорь', 'surname': 'Акинфеев', 'patronymic': 'Владимирович'},
+            {'name': 'Александр', 'surname': 'Овечкин', 'patronymic': 'Михайлович'},
+            {'name': 'Олег', 'surname': 'Тиньков', 'patronymic': 'Юрьевич'},
+            {'name': 'Ольгa', 'surname': 'Наумовa', 'patronymic': 'Валерьевна'},
+            {'name': 'Андрей', 'surname': 'Андреев', 'patronymic': 'Вагнерович'},
+            {'name': 'Алексей', 'surname': 'Картынник', 'patronymic': ''},
+            {'name': 'Игорь', 'surname': 'Бухман', 'patronymic': 'Анатольевич'},
+            {'name': 'Юрий', 'surname': 'Дудь', 'patronymic': 'Александрович'},
+            {'name': 'Григорий', 'surname': 'Перельман', 'patronymic': 'Яковлевич'}]
+
+def decide_person(text):
+    for i in range(len(names_mask)):
+        person = names_mask[i]
+        if person['surname'] in text:
+            return names[i]
+    return None
+
 def main():
     filenames = glob('input_html/*')
     for filename in filenames:
@@ -20,7 +49,8 @@ def main():
         for script in soup(["script", "style"]):
             script.extract()
         text = soup.get_text()
-        print(text)
+        person = decide_person(text)
+        print(person)
         file.close()
 
 if __name__ == '__main__':
