de1ea253778c6b0fbdfe627c3ca0dd710c287dba
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-12-03 21:20:49 +0300

    rewriting for direct file access

--- mapper.py
+++ mapper.py
@@ -1,15 +1,27 @@
 #!/bin/python3
 import sys
 from bs4 import BeautifulSoup
+from glob import glob
 
 def main():
-    for page in sys.stdin:
-        soup = BeautifulSoup(page, features="html.parser")
+    filenames = glob('input_html/*')
+    for filename in filenames:
+        print('Processing ' + filename)
+        file = open(filename, 'r') 
+        try:
+            page = file.read()
+        except UnicodeDecodeError:
+            file.close()
+            continue
+        link = page.split('\n')[1].split('saved from url')[1].split(')')[1].split()[0]
+        print(link)
 
+        soup = BeautifulSoup(page, features="html.parser")
         for script in soup(["script", "style"]):
             script.extract()
         text = soup.get_text()
         print(text)
+        file.close()
 
 if __name__ == '__main__':
     main()
