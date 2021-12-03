adb0f4a5540a4a9759df084d9ff54268c725a0eb
Aleksei Volkov <volkov.aa@phystech.edu>
2021-12-04 00:35:10 +0300

    Changed absolute path to relative

--- copy_html.sh
+++ copy_html.sh
@@ -5,7 +5,7 @@ ssh -p 20000 team5@176.118.164.173 hdfs dfs -mkdir -p input_html
 ssh -p 20000 team5@176.118.164.173 hdfs dfs -cp input/*.html input_html
 ssh -p 20000 team5@176.118.164.173 hdfs dfs -get input_html
 ssh -p 20000 team5@176.118.164.173 hdfs dfs -rm -R input_html
-scp -r -P 20000 team5@176.118.164.173:/home/team5/input_html /root/input_html
+scp -r -P 20000 team5@176.118.164.173:/home/team5/input_html input_html
 ssh -p 20000 team5@176.118.164.173 rm -rf input_html
 echo "Files succesfully copied!"
 
