e752f00ef0ec7024ca355621da5b36d61b27490d
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-12-03 22:02:38 +0300

    hdfs script

---
+++ copy_html.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Moving html from input/ to input_html/"
+ssh -p 20000 team5@176.118.164.173 hdfs dfs -mkdir -p input_html
+ssh -p 20000 team5@176.118.164.173 hdfs dfs -cp input/*.html input_html
+ssh -p 20000 team5@176.118.164.173 hdfs dfs -get input_html
+ssh -p 20000 team5@176.118.164.173 hdfs dfs -rm -R input_html
+scp -r -P 20000 team5@176.118.164.173:/home/team5/input_html /root/input_html
+ssh -p 20000 team5@176.118.164.173 rm -rf input_html
+echo "Files succesfully copied!"
+
